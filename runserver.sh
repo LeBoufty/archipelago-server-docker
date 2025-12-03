354cea92337e1e8ed56d20d444210bc931e9c949
LeBoufty <boufto@gmail.com>
2025-12-03 20:54:58 +0100

    forgot to create folder

--- runserver.sh
+++ runserver.sh
@@ -7,6 +7,7 @@ rm -f *.archipelago
 
 cd output && unzip -j AP_*.zip
 cp *.archipelago ../game.archipelago
+mkdir -p static
 rm -f static/spoiler.txt
 cp *_Spoiler.txt static/spoiler.txt
 cp AP_*.zip static/game.zip
