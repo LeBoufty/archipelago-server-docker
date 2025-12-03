835180d439badc71ac4aeb09df2fcd4774ee36b4
LeBoufty <boufto@gmail.com>
2025-12-03 20:51:59 +0100

    added a static folder for set names

--- runserver.sh
+++ runserver.sh
@@ -7,6 +7,9 @@ rm -f *.archipelago
 
 cd output && unzip -j AP_*.zip
 cp *.archipelago ../game.archipelago
+rm -f static/spoiler.txt
+cp *_Spoiler.txt static/spoiler.txt
+cp AP_*.zip static/game.zip
 
 cd ..
 
